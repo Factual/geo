d5e780dbff7aeefef443c4390cc026c1107bcf28
Horace Williams <horace.d.williams@gmail.com>
2018-06-26 09:00:43 -0700

    Push 2.0.0-rc-1 release version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  factual/geo "1.2.2-SNAPSHOT"
+  factual/geo "2.0.0-rc-1"
   :url     "https://github.com/factual/geo"
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"
