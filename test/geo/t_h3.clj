b63975ee208975e295d980a661fd0c655cb8642a
Will Cohen <willcohen@users.noreply.github.com>
2018-07-02 16:20:30 -0400

    Add h3 tests

---
+++ test/geo/t_h3.clj
@@ -0,0 +1,53 @@
+(ns geo.t-h3
+  (:require [geo.h3 :as sut]
+            [geo.geohash :as geohash]
+            [geo.jts :as jts]
+            [geo.spatial :as spatial]
+            [midje.sweet :refer [fact facts]])
+  (:import (org.locationtech.jts.geom Geometry LinearRing)
+           (com.uber.h3core.util GeoCoord)))
+
+(facts "h3 core functions"
+       (fact "from coordinates"
+             (sut/pt->h3 57.64911063015461 10.407439693808556 7) => "871f24ac5ffffff")
+       (fact "from point"
+             (sut/pt->h3 (jts/point 57.64911063015461 10.407439693808556) 7) => "871f24ac5ffffff")
+       (fact "strings and longs"
+             (sut/string->long "871f24ac5ffffff") => 608533827635118079
+             (sut/long->string 608533827635118079) => "871f24ac5ffffff")
+       (fact "resolution"
+             (sut/get-resolution "871f24ac5ffffff") => 7
+             (sut/get-resolution (sut/string->long "871f24ac5ffffff")) => 7)
+       (fact "boundary"
+             (type (sut/jts-boundary "871f24ac5ffffff")) => LinearRing)
+       (fact "geo coord"
+             (type (first (sut/geo-coords (geohash/geohash "u4pruy")))) => GeoCoord))
+
+
+(facts "h3 algorithms"
+       (fact "ring"
+             (sut/ring "871f24ac5ffffff" 1) => [["871f24ac5ffffff"]
+                                                ["871f24ac4ffffff" "871f24ac0ffffff" "871f24ac1ffffff"
+                                                 "871f24aeaffffff" "871f24aeeffffff" "871f24ae3ffffff"]]
+             (sut/ring-cells "871f24ac5ffffff" 1) => ["871f24ac5ffffff" "871f24ac4ffffff" "871f24ac0ffffff"
+                                                      "871f24ac1ffffff" "871f24aeaffffff" "871f24aeeffffff"
+                                                      "871f24ae3ffffff"])
+       (fact "polyfill"
+             (sut/polyfill (geohash/geohash "u4pruy") 9) => ["891f24ac54bffff"
+                                                             "891f24ac097ffff"
+                                                             "891f24ac0b3ffff"
+                                                             "891f24ac543ffff"
+                                                             "891f24ac55bffff"]
+             (count (sut/polyfill
+                      (jts/set-srid (.difference (spatial/to-jts (geohash/geohash "u4pruy"))
+                                                 (spatial/to-jts (geohash/geohash "u4pruyk")))
+                                    jts/default-srid) 10)) => 35)
+       (fact "compact/uncompact"
+             (count (sut/compact (sut/polyfill
+                                   (jts/set-srid (.difference (spatial/to-jts (geohash/geohash "u4pruy"))
+                                                              (spatial/to-jts (geohash/geohash "u4pruyk")))
+                                                 jts/default-srid) 10))) => 23
+             (count (sut/uncompact (sut/polyfill
+                                     (jts/set-srid (.difference (spatial/to-jts (geohash/geohash "u4pruy"))
+                                                                (spatial/to-jts (geohash/geohash "u4pruyk")))
+                                                   jts/default-srid) 10) 11)) => 245))
