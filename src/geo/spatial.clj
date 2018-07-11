ac9f062a0026028f8bd9509417117b9889be54b4
Will Cohen <willcohen@users.noreply.github.com>
2018-07-11 15:36:22 -0400

    Use transform-geom on the lat/lng checks for JTS points

--- src/geo/spatial.clj
+++ src/geo/spatial.clj
@@ -141,8 +141,8 @@
   (to-geohash-point [this] this)
 
   org.locationtech.jts.geom.Point
-  (latitude [this] (.getY (to-jts this jts/default-srid)))
-  (longitude [this] (.getX (to-jts this jts/default-srid)))
+  (latitude [this] (.getY (jts/transform-geom this jts/default-srid)))
+  (longitude [this] (.getX (jts/transform-geom this jts/default-srid)))
   (to-spatial4j-point [this] (spatial4j-point this))
   (to-geohash-point [this] (geohash-point this))
 
