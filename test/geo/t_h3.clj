f670ffaa7d171d2af7de44609dcc6eb0ceea1a93
Will Cohen <willcohen@users.noreply.github.com>
2018-07-16 12:41:27 -0400

    Refactor h3 tests to use var for h3 identifier

--- test/geo/t_h3.clj
+++ test/geo/t_h3.clj
@@ -11,20 +11,24 @@
 (def geohash-with-hole (jts/set-srid (.difference (spatial/to-jts (geohash/geohash "u4pruy"))
                                                   (spatial/to-jts (geohash/geohash "u4pruyk")))
                                      jts/default-srid))
+(def h3-example-str "871f24ac5ffffff")
+(def h3-example-long 608533827635118079)
+(def h3-example-latitude 57.64911063015461)
+(def h3-example-longitude 10.407439693808556)
 
 (facts "h3 core functions"
        (fact "from coordinates"
-             (sut/pt->h3 57.64911063015461 10.407439693808556 7) => "871f24ac5ffffff")
+             (sut/pt->h3 h3-example-latitude h3-example-longitude 7) => h3-example-str)
        (fact "from point"
-             (sut/pt->h3 (jts/point 57.64911063015461 10.407439693808556) 7) => "871f24ac5ffffff")
+             (sut/pt->h3 (jts/point h3-example-latitude h3-example-longitude) 7) => h3-example-str)
        (fact "strings and longs"
-             (sut/to-long "871f24ac5ffffff") => 608533827635118079
-             (sut/to-string 608533827635118079) => "871f24ac5ffffff")
+             (sut/to-long h3-example-str) => h3-example-long
+             (sut/to-string h3-example-long) => h3-example-str)
        (fact "resolution"
-             (sut/get-resolution "871f24ac5ffffff") => 7
-             (sut/get-resolution (sut/to-long "871f24ac5ffffff")) => 7)
+             (sut/get-resolution h3-example-str) => 7
+             (sut/get-resolution (sut/to-long h3-example-str)) => 7)
        (fact "jts boundary"
-             (type (sut/to-jts "871f24ac5ffffff")) => Polygon)
+             (type (sut/to-jts h3-example-str)) => Polygon)
        (fact "geo coord"
              (type (first (sut/geo-coords (geohash/geohash "u4pruy")))) => GeoCoord)
        (fact "edges"
@@ -35,15 +39,15 @@
                                                "1471f24ac4ffffff" "1571f24ac4ffffff" "1671f24ac4ffffff"]
              (type (first (sut/edge-boundary "1371f24ac4ffffff"))) => GeoCoord)
        (fact "h3->pt"
-             (str (spatial/to-jts (sut/h3->pt "871f24ac5ffffff")))
+             (str (spatial/to-jts (sut/h3->pt h3-example-str)))
              => "POINT (10.423520614389421 57.65506363212537)")
        (fact "pentagon"
              (sut/pentagon? "8f28308280f18f2") => falsey
              (sut/pentagon? "821c07fffffffff") => truthy)
        (fact "validity checks"
-             (sut/is-valid? 608533827635118079) => truthy
+             (sut/is-valid? h3-example-long) => truthy
              (sut/is-valid? -1) => falsey
-             (sut/is-valid? "871f24ac5ffffff") => truthy)
+             (sut/is-valid? h3-example-str) => truthy)
        (fact "neighbor checks"
              (sut/neighbors? "871f24ac4ffffff" "871f24ac0ffffff") => truthy
              (sut/neighbors? 608533827618340863 608533827551231999) => truthy
@@ -52,12 +56,12 @@
 
 (facts "h3 algorithms"
        (fact "rings"
-             (sut/hex-ring "871f24ac5ffffff" 0) => ["871f24ac5ffffff"]
-             (sut/hex-ring "871f24ac5ffffff" 1) => ["871f24ae3ffffff" "871f24ac4ffffff" "871f24ac0ffffff"
-                                                    "871f24ac1ffffff" "871f24aeaffffff" "871f24aeeffffff"]
-             (sut/hex-range "871f24ac5ffffff" 1) => [["871f24ac5ffffff"]
-                                                     ["871f24ac4ffffff" "871f24ac0ffffff" "871f24ac1ffffff"
-                                                      "871f24aeaffffff" "871f24aeeffffff" "871f24ae3ffffff"]])
+             (sut/hex-ring h3-example-str 0) => ["871f24ac5ffffff"]
+             (sut/hex-ring h3-example-str 1) => ["871f24ae3ffffff" "871f24ac4ffffff" "871f24ac0ffffff"
+                                                 "871f24ac1ffffff" "871f24aeaffffff" "871f24aeeffffff"]
+             (sut/hex-range h3-example-str 1) => [["871f24ac5ffffff"]
+                                                  ["871f24ac4ffffff" "871f24ac0ffffff" "871f24ac1ffffff"
+                                                   "871f24aeaffffff" "871f24aeeffffff" "871f24ae3ffffff"]])
        (fact "polyfill"
              (sut/polyfill (geohash/geohash "u4pruy") 9) => ["891f24ac54bffff"
                                                              "891f24ac097ffff"
@@ -78,7 +82,7 @@
              (count (jts/coordinates
                       (sut/multi-polygon (sut/polyfill geohash-with-hole 12)))) => 402)
        (fact "interoperability with geometry collections"
-             (->> (sut/hex-range "871f24ac5ffffff" 3)
+             (->> (sut/hex-range h3-example-str 3)
                   (map sut/multi-polygon)
                   jts/geometry-collection
                   io/to-features
