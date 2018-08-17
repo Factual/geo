5d6e9200f95ef840e2deaec7c54d625a91d3162e
Horace Williams <horace.d.williams@gmail.com>
2018-08-17 10:36:13 -0700

    2.1.0 release

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  factual/geo "2.1.0-rc-1"
+  factual/geo "2.1.0"
   :url     "https://github.com/factual/geo"
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"
