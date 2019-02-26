7113de77af2a9b85e21ff2285b3942385b0bb8cc
Horace Williams <horace.d.williams@gmail.com>
2019-02-25 16:46:55 -0800

    3.0.0-rc-2 automated release test

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject
-  factual/geo "3.0.0-rc-1"
+  factual/geo "3.0.0-rc-2"
   :url     "https://github.com/factual/geo"
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"
